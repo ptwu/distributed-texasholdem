434169bd031c8a2bd7851402c26e142029332d97
Aurelien <aurelien@identity-labs.fr>
2020-06-13 16:01:30 +0200

    Test parametable small/big blind

--- test/classes/game.test.js
+++ test/classes/game.test.js
@@ -3,6 +3,8 @@ const events = require('events');
 
 test('Test call until fold then check', () => {
   const game = new Game('best-game', '1');
+  game.smallBlind = 5;
+  game.bigBlind = 10;
 
   // Mock socket
   const sock1 = new events.EventEmitter();
@@ -11,7 +13,9 @@ test('Test call until fold then check', () => {
   sock2.id = 2;
 
   const p1 = game.addPlayer("1", sock1);
+  expect(p1.money).toBe(100);
   const p2 = game.addPlayer("2", sock2);
+  expect(p2.money).toBe(100);
 
   expect(game.findPlayer(1)).toBe(p1);
   expect(game.findPlayer(2)).toBe(p2);
@@ -23,9 +27,14 @@ test('Test call until fold then check', () => {
   game.startGame();
   expect(game.roundNum).toBe(1);
   expect(game.roundData.bets.length).toBe(1);
+
   
   const smallPlayer = game.players[game.roundData.smallBlind];
   const bigPlayer = game.players[game.roundData.bigBlind];
+
+  // Test parametable small/big blind
+  expect(smallPlayer.money).toBe(95);
+  expect(bigPlayer.money).toBe(90);
 
   // Pre-Flop
   game.call(smallPlayer.socket);
