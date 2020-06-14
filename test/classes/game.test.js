3e49c403d380e61b92a21303ba072c8f896bc43a
Aurelien <aurelien@identity-labs.fr>
2020-06-14 21:13:47 +0200

    Fix conflict in test

--- test/classes/game.test.js
+++ test/classes/game.test.js
@@ -264,12 +264,7 @@ test('Test disconnected', () => {
   expect(game.players.length).toBe(1);
 });
 
-
-<<<<<<< HEAD
-test('Test raise', () => {
-=======
 test('Test init blind and dealer', () => {
->>>>>>> 9f9a74376b1087be91363b2800ca5e1685a4dc70
   const game = new Game('best-game', '1');
   game.smallBlind = 5;
   game.bigBlind = 10;
@@ -279,41 +274,6 @@ test('Test init blind and dealer', () => {
   sock1.id = 1;
   const sock2 = new events.EventEmitter();
   sock2.id = 2;
-<<<<<<< HEAD
-
-  const p1 = game.addPlayer("1", sock1);
-  expect(p1.money).toBe(100);
-  const p2 = game.addPlayer("2", sock2);
-  expect(p2.money).toBe(100);
-
-  expect(game.findPlayer(1)).toBe(p1);
-  expect(game.findPlayer(2)).toBe(p2);
-
-  expect(game.players.length).toBe(2);
-  
-  expect(game.roundNum).toBe(0);
-  expect(game.roundData.bets.length).toBe(0);
-  game.startGame();
-  expect(game.roundNum).toBe(1);
-  expect(game.roundData.bets.length).toBeGreaterThan(0);
-
-  let currentPlayer;
-  
-  // Pre-Flop
-  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
-  expect(game.bet(currentPlayer.socket, 30)).toBe(true);
-
-  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
-  // Check can't raise under topBet
-  expect(game.raise(currentPlayer.socket, 20)).not.toBe(true);
-  expect(game.roundNum).toBe(1);
-  expect(game.roundData.bets.length).toBe(1);
-
-  // Raise correct value
-  expect(game.raise(currentPlayer.socket, 30)).toBe(true);
-  expect(game.roundNum).toBe(1);
-  expect(game.roundData.bets.length).toBe(2);
-=======
   const sock3 = new events.EventEmitter();
   sock3.id = 3;
 
@@ -367,5 +327,49 @@ test('Test init blind and dealer', () => {
   expect(p1.blindValue).toBe('');
   expect(p2.blindValue).toBe('Small Blind');
   expect(p3.blindValue).toBe('Big Blind');
->>>>>>> 9f9a74376b1087be91363b2800ca5e1685a4dc70
+});
+
+test('Test raise', () => {
+  const game = new Game('best-game', '1');
+  game.smallBlind = 5;
+  game.bigBlind = 10;
+
+  // Mock socket
+  const sock1 = new events.EventEmitter();
+  sock1.id = 1;
+  const sock2 = new events.EventEmitter();
+  sock2.id = 2;
+
+  const p1 = game.addPlayer("1", sock1);
+  expect(p1.money).toBe(100);
+  const p2 = game.addPlayer("2", sock2);
+  expect(p2.money).toBe(100);
+
+  expect(game.findPlayer(1)).toBe(p1);
+  expect(game.findPlayer(2)).toBe(p2);
+
+  expect(game.players.length).toBe(2);
+  
+  expect(game.roundNum).toBe(0);
+  expect(game.roundData.bets.length).toBe(0);
+  game.startGame();
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBeGreaterThan(0);
+
+  let currentPlayer;
+  
+  // Pre-Flop
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  expect(game.bet(currentPlayer.socket, 30)).toBe(true);
+
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  // Check can't raise under topBet
+  expect(game.raise(currentPlayer.socket, 20)).not.toBe(true);
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBe(1);
+
+  // Raise correct value
+  expect(game.raise(currentPlayer.socket, 30)).toBe(true);
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBe(2);
 });
