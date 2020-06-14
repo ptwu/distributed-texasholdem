05654764f41a2a77ea2707732045fc5294a94e2e
Aurelien <aurelien@identity-labs.fr>
2020-06-14 15:11:04 +0200

    Rework of asignation of small/bigBlind + adding dealer

--- test/classes/game.test.js
+++ test/classes/game.test.js
@@ -263,3 +263,57 @@ test('Test disconnected', () => {
 
   expect(game.players.length).toBe(1);
 });
+
+
+test('Test init blind and dealer', () => {
+  const game = new Game('best-game', '1');
+  game.smallBlind = 5;
+  game.bigBlind = 10;
+
+  // Mock socket
+  const sock1 = new events.EventEmitter();
+  sock1.id = 1;
+  const sock2 = new events.EventEmitter();
+  sock2.id = 2;
+  const sock3 = new events.EventEmitter();
+  sock3.id = 3;
+
+  const p1 = game.addPlayer("1", sock1);
+  const p2 = game.addPlayer("2", sock2);
+  const p3 = game.addPlayer("3", sock3);
+
+  expect(game.findPlayer(1)).toBe(p1);
+  expect(game.findPlayer(2)).toBe(p2);
+  expect(game.findPlayer(3)).toBe(p3);
+
+  expect(game.players.length).toBe(3);
+  
+  expect(game.roundNum).toBe(0);
+  game.startGame();
+  expect(game.roundNum).toBe(1);
+
+  expect(p1.dealer).toBe(true);
+  expect(p2.blindValue).toBe('Small Blind');
+  expect(p3.blindValue).toBe('Big Blind');
+
+  game.startNewRound();
+  expect(game.roundNum).toBe(2);
+
+  expect(p2.dealer).toBe(true);
+  expect(p3.blindValue).toBe('Small Blind');
+  expect(p1.blindValue).toBe('Big Blind');
+
+  game.startNewRound();
+  expect(game.roundNum).toBe(3);
+
+  expect(p3.dealer).toBe(true);
+  expect(p1.blindValue).toBe('Small Blind');
+  expect(p2.blindValue).toBe('Big Blind');
+
+  game.startNewRound();
+  expect(game.roundNum).toBe(4);
+
+  expect(p1.dealer).toBe(true);
+  expect(p2.blindValue).toBe('Small Blind');
+  expect(p3.blindValue).toBe('Big Blind');
+});
