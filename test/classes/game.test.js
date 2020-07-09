aebacab3dbcd9d3ffeda17e33a77177dedadea6d
Aurelien <aurelien@identity-labs.fr>
2020-07-09 10:51:18 +0200

    Add test all fold

--- test/classes/game.test.js
+++ test/classes/game.test.js
@@ -719,6 +719,67 @@ test('Test fold', () => {
   expect(game.players.reduce((a, c) => a + c.money, 0)).toBe(300);
 });
 
+test('Test all fold', () => {
+  const game = new Game('best-game', '1');
+  game.smallBlind = 5;
+  game.bigBlind = 10;
+
+  // Mock socket
+  const sock1 = new events.EventEmitter();
+  sock1.id = 1;
+  const sock2 = new events.EventEmitter();
+  sock2.id = 2;
+  const sock3 = new events.EventEmitter();
+  sock3.id = 3;
+
+  const p1 = game.addPlayer("1", sock1);
+  expect(p1.money).toBe(100);
+
+  const p2 = game.addPlayer("2", sock2);
+  expect(p2.money).toBe(100);
+
+  const p3 = game.addPlayer("3", sock3);
+  expect(p3.money).toBe(100);
+
+  expect(game.findPlayer(1)).toBe(p1);
+  expect(game.findPlayer(2)).toBe(p2);
+  expect(game.findPlayer(3)).toBe(p3);
+
+  expect(game.players.length).toBe(3);
+  
+  expect(game.roundNum).toBe(0);
+  expect(game.roundData.bets.length).toBe(0);
+  game.startGame();
+
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBeGreaterThan(0);
+
+  let currentPlayer;
+  
+  // Pre-Flop
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  expect(game.raise(currentPlayer.socket, 30)).toBe(true);
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  expect(game.call(currentPlayer.socket)).toBe(true);
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  expect(game.call(currentPlayer.socket)).toBe(true);
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBe(2);
+  expect(game.roundInProgress).toBe(true);
+
+  // Flop
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  game.fold(currentPlayer.socket);
+  currentPlayer = game.players.filter((p) => p.status === 'Their Turn')[0];
+  game.fold(currentPlayer.socket);
+
+  expect(game.roundNum).toBe(1);
+  expect(game.roundData.bets.length).toBe(2);
+  expect(game.roundInProgress).toBe(false);
+
+  expect(game.players.reduce((a, c) => a + c.money, 0)).toBe(300);
+});
+
 test('Test _distributeMoney', () => {
   const game = new Game('best-game', '1');
   const pot = 100;
